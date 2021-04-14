6859832a5f3350bce862836bd727afeb383f0485
Sunil Jadhav <sunijadh@in.ibm.com>
2021-04-14 13:55:36 +0530

    Second Commit

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ app.get("/", (req, res) => {
 });
 
 // Get all rows from the table test_table
-router.get('/test_table', routes.test.getAll);
+router.get('/getAllTables', routes.routeController.getAll);
 
-app.use('/api', router);
+// Dynamically get specific table
+router.get('/get/:table', routes.routeController.getSpecificTable);
+
+// Dynamically get specific table and record
+router.get('/get/:table/:id', routes.routeController.getSpecificRecord);
+
+app.use('/api/v1', router);
 
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
-const test = require('./testController');
+const routeController = require('./routeController');
 
 module.exports = {
-    test
+    routeController
 }

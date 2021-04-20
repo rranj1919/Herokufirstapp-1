e33bd16414826bc98021efc17fe79d2dd6280fff
Sebastian Blomkvist <Sebastian.Blomkvist-CIC@ibm.com>
2021-04-20 14:10:23 +0200

    auth encrypt

--- auth.js
+++ auth.js
@@ -1,9 +1,15 @@
-// Authorize request
+// Authorize request middleware - used in REST routes
 module.exports = function(req, res, next) {
     // Check if credentials are missing
     if(!req.header('username') || !req.header('password')) {
         return res.status(401).json({ msg: 'Missing Credentials' })
     }
+
+    console.log('Auth header: ', req.header('Authorization'))
+
+    const buff = Buffer.from(base64, 'base64');
+    const authStr = buff.toString('utf-8');
+    console.log('Decoded: ', authStr);
 
     // Try to check if credentials are correct
     try {

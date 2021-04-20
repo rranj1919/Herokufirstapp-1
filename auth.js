c302f50fea193f67c94ef959496649500d94bd50
Sebastian Blomkvist <Sebastian.Blomkvist-CIC@ibm.com>
2021-04-20 14:35:40 +0200

    auth encrypt

--- auth.js
+++ auth.js
@@ -1,18 +1,24 @@
 // Authorize request middleware - used in REST routes
 module.exports = function(req, res, next) {
+    // Check base64 encoded 'Authorization' header
+    try {
+        // Add .slice(6) to remove postman's 'Basic ' in begining of auth header
+        const buff = Buffer.from(req.header('Authorization').slice(6), 'base64');
+        const authStr = buff.toString('utf-8');
+
+        // ['username', 'password']
+        const splitAuthStr = authStr.split(':');
+
+        // TODO (if required) - check credentials with heroku config vars  
+    } catch(err) {
+        console.log('No Encoded Authorization Header')
+        // res.status(401).json({ msg: 'Unauthorized' })
+    }
+    
     // Check if credentials are missing
     if(!req.header('username') || !req.header('password')) {
         return res.status(401).json({ msg: 'Missing Credentials' })
     }
-
-    console.log('Auth header: ', req.header('Authorization'))
-
-    const buff = Buffer.from(req.header('Authorization').slice(6), 'base64');
-    const authStr = buff.toString('utf-8');
-    console.log('Decoded: ', authStr);
-
-    const splitAuthStr = authStr.split(':');
-    console.log('auth array: ', splitAuthStr);
 
     // Try to check if credentials are correct
     try {

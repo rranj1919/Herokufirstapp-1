329d5b90db0e71b4c4f3654906d38a377da5249e
akshaysphadnis <akshaysphadnis@gmail.com>
2021-05-10 13:31:19 +0530

    Updated Code

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const getAllTables = async (args, cb, headers) => {
             error: 'Unauthorized User'
         }
     }
-
+	
     // If here - user has provided correct credentials and is authorized
     console.log("User Authorized")
 
@@ -103,7 +103,7 @@ const getAllTables = async (args, cb, headers) => {
         // Loop through all tables and query them - save in results obj
         for(const table of tableArray) {
             console.log('Querying table: ', table.table_name);
-            let result = await client.query(`SELECT * FROM ${table.table_name} ${queryWhere}`)
+            let result = await client.query(`SELECT * FROM ${table.table_name}`)
             let resultsArray = result.rows;
             for (const result of resultsArray){
                 // loop through all object fields and check for fields with objects (date)

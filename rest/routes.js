702c7a5de724b5c1856273afa20925b7bc9b8d8c
Sebastian Blomkvist <Sebastian.Blomkvist-CIC@ibm.com>
2021-05-25 15:05:40 +0200

    schema param

--- rest/routes.js
+++ rest/routes.js
@@ -85,7 +85,7 @@ router.get('/get', auth, async (req, res) => {
                                                 FROM information_schema.tables
                                                 WHERE table_schema='${schema}'
                                                 AND table_type='BASE TABLE'
-                                                AND (table_name NOT LIKE '_hc%' OR table_name NOT LIKE '_sf%' OR table_name NOT LIKE '_tr%'`);
+                                                AND (table_name NOT LIKE '_hc%' OR table_name NOT LIKE '_sf%' OR table_name NOT LIKE '_tr%')`);
             const tableArray = tableQuery.rows;
             console.log(tableArray);
             results = {};
